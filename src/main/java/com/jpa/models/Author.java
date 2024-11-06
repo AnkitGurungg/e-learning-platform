9550b9db9c5540b5d3b2a4760eccb889c355cee1
AnkitGurung <ankitgurung234@gmail.com>
2024-11-06 11:25:19 +0545

    Create Author class; Use edit configuration to set active profiles and allow create tables in two databases PostgreSQl and MySQL one by one; Use Lombok to reduce boilerplate code; Develop specific sequence for each entity using @SequenceGenerator annotation

--- src/main/java/com/jpa/models/Author.java
+++ src/main/java/com/jpa/models/Author.java
@@ -1,7 +1,24 @@
 package com.jpa.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class Author {
 
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "author_sequence"
+    )
+    @SequenceGenerator(
+            name = "author_sequence",
+            sequenceName = "author_sequence",
+            allocationSize = 1
+    )
     private Integer id;
     private String firstName;
     private String lastName;
