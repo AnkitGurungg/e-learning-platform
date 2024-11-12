9242edbf1081699c69692525fed1a10b7eb874c0
AnkitGurung <ankitgurung234@gmail.com>
2024-11-12 23:00:53 +0545

    Use table per class strategy in JPA/Hibernate to implement inheritance hierarchy where each class will have its own table with a copy of all the properties inherited from base class

--- src/main/java/com/jpa/models/Video.java
+++ src/main/java/com/jpa/models/Video.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.Polymorphism;
+import org.hibernate.annotations.PolymorphismType;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -13,8 +15,9 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @SuperBuilder
 @Entity
-@PrimaryKeyJoinColumn(name = "video_id")
+//@PrimaryKeyJoinColumn(name = "video_id") // For JOINT_STRATEGY
 //@DiscriminatorValue("V")
+@Polymorphism(type = PolymorphismType.EXPLICIT) // Use in TABLE_PER_CLASS to explicit Video table while querying Resource table
 public class Video extends Resource {
 
     private int length;
