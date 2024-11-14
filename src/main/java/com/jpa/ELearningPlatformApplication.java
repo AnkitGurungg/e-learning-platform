c967b8628e0f4843cfaa1f41a1aeb527906981e9
AnkitGurung <ankitgurung234@gmail.com>
2024-11-15 02:08:56 +0545

    Use Spring Data JPA to dynamically generate query generation or derivate query at the run time; Create custom queries; Use fake data generated by javafaker to test derivate query and custom query methods of AuthorRepository

--- src/main/java/com/jpa/ELearningPlatformApplication.java
+++ src/main/java/com/jpa/ELearningPlatformApplication.java
@@ -1,6 +1,7 @@
 package com.jpa;
 
-import com.jpa.models.Video;
+import com.github.javafaker.Faker;
+import com.jpa.models.Author;
 import com.jpa.repositories.AuthorRepository;
 import com.jpa.repositories.VideoRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -21,7 +22,35 @@ public class ELearningPlatformApplication {
             VideoRepository videoRepository
     ) {
         return args -> {
+            for(int i=0; i<50; i++) {
+                Faker faker = new Faker();
+                var author = Author.builder()
+                        .firstName(faker.name().firstName())
+                        .lastName(faker.name().lastName())
+                        .age(faker.number().numberBetween(20, 80))
+                        .email(faker.name().username() + "ankitgurung@gmail.com")
+                        .build();
+                repository.save(author);
+            }
+
+//            update all values of author with id=1
+            var author = Author.builder()
+                .id(1)
+                .firstName("Ankit")
+                .lastName("Gurung")
+                .age(16)
+                .email("ankitgurung@gmail.com")
+                .build();
+//            repository.save(author);
+
+//            update Author a set a.age = 22 where a.id = 1 or update specific value(age) of author where id = 1
+//            repository.updateAuthor(22, 1);
+
+//            update all authors
+            repository.updateAllAuthors(99);
+
             /*
+//            Save Author object
             var author = Author.builder()
                     .firstName("Ankit")
                     .lastName("Gurung")
@@ -31,12 +60,15 @@ public class ELearningPlatformApplication {
             repository.save(author);
             */
 
+            /*
+//            Save Video object
             var video = Video.builder()
                     .name("abc")
                     .size(5)
                     .length(10)
                     .build();
             videoRepository.save(video);
+            */
         };
     }
 
--- src/main/java/com/jpa/repositories/AuthorRepository.java
+++ src/main/java/com/jpa/repositories/AuthorRepository.java
@@ -1,8 +1,46 @@
 package com.jpa.repositories;
 
 import com.jpa.models.Author;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-public interface AuthorRepository extends JpaRepository<Author, Integer> {
+import java.util.List;
+
+public interface AuthorRepository extends JpaRepository<Author, Integer>, JpaSpecificationExecutor<Author> {
+    // Property, Condition and Refinement
+
+    // Custom queries
+    // Executing an update/delete query error, because the updateAuthor method is not transactional and to fix that make
+    // the updateAuthor method transactional because the modification or the update need to be within same transaction
+    @Modifying
+    @Transactional
+    @Query("update Author a set a.age = :age where a.id = :id")
+    int updateAuthor(int age, int id);
+
+    @Modifying
+    @Transactional
+    @Query("update Author a set a.age = :age")
+    void updateAllAuthors(int age);
+
+    // select * from author where first_name = 'ankit'
+    List<Author> findAllByFirstName(String fn);
+
+    // select * from author where first_name = 'ankit' or 'Ankit'
+    List<Author> findAllByFirstNameIgnoreCase(String fn);
+
+    // select * from author where first_name like '%an%'
+    List<Author> findAllByFirstNameContainingIgnoreCase(String fn);
+
+    // select * from author where first_name like 'an%'
+    List<Author> findAllByFirstNameStartsWithIgnoreCase(String fn);
+
+    // select * from author where first_name like '%an'
+    List<Author> findAllByFirstNameEndsWithIgnoreCase(String fn);
+
+    // select * from author where first_name in ('ankit', 'gurung', 'codes')
+    List<Author> findAllByFirstNameInIgnoreCase(List<String> firstNames);
 
 }
